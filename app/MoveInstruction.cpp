50cdf88880f364dee8657ba8aa4c4707f28b6a53
Mason Wong <masonjw1@uci.edu>
2023-10-30 13:01:14 -0700

    moving old-state into main

--- app/MoveInstruction.cpp
+++ app/MoveInstruction.cpp
@@ -11,24 +11,6 @@ void MoveInstruction::execute(ProgramState * state) const
     // Get register index
     std::cout << destination << " " << source << std::endl;
     int registerIndex = state->getRegisterIndex(destination);
-    // Check source
-    int registerIndex2 = checkSource(state, source);
     // Store it into registerVector
-    state->storeRegisterValue(registerIndex, registerIndex2);
-}
-
-// checks if the source is a digit or a register
-int MoveInstruction::checkSource(ProgramState * state, std::string source) const
-{
-    int registerIndex2;
-	// Check if source is a digit, or another register
-    if (isdigit(source[0]))
-    {
-        registerIndex2 = std::stoi(source);
-    }
-    else
-    {
-        registerIndex2 = state->getRegisterIndex(source);
-    }
-    return registerIndex2;
+    state->storeRegisterValue(registerIndex, source);
 }
